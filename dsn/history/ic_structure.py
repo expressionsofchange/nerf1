expressionsofchange/nerf1

commit ae8b843b281ef681d9814df87f4e509ad31b3ec9

(Breaking refactoring) trees broadcast t-address
In working towards animations that display what's actually going on on the
history-side when selecting a different element in the tree, a
refactoring was needed:

Previous to the refactoring, the tree would communicate a cursor-change
to the history-widget by sending the full new score to display (with
some additional indirection as well). When a deeper node in the tree was
selected, the score of that node would be sent to the history, and the
history would render it.

However, we want to make some "animations" on the history-widget
(visualisations of state transitions) that show how pre-cursor-change
and post-cursor-change relate. To do this, the history-widget needs to
know this relationship. That is: it needs to have access to the global
history as well as the information that only part of that global history
must be displayed.

To achieve that, we've split out the 2 pieces of information into 2
channels and associated send methods: one for the full score (of all
history) and one for the cursor changes; The former is connected in
editor.py, by recycling the child-channel creation mechanism; the latter
is using a much simplified version of broadcast_cursor_update.

The refactoring also removes quite some complexity that was put in place
with the specific goal of not sharing the t-address space from the tree
to the history widget. (i.e. it removes the aformementioned indirection)

Breaking refactoring, because the history no longer shows what's under the cursor.
We will "fix this soon"

widgets/history.py
@@ -88,22 +88,14 @@ class HistoryWidget(FocusBehavior, Widget):
         self.bind(size=self.invalidate)
 
     def parent_cursor_update(self, data):
-        do_create = data
-
-        if self.data_channel is not None:
-            self.close_channel()
-
-        self.data_channel, do_kickoff = do_create()
-        self.send_to_channel, self.close_channel = self.data_channel.connect(self.receive_from_parent)
-
-        # If we're bound to a different s_cursor in the parent tree, we unconditionally reset our own cursor:
+        print("received t-address", data)
+        """
         self.ds = EHStructure(
             self.ds.score,
             List([n.to_s_expression() for n in self.ds.score.notes()]),
-            [0],
+            [0], # If we're bound to a different s_cursor in the parent tree, we unconditionally reset our own cursor
         )
-
-        do_kickoff()
+        """
 
     def receive_from_parent(self, data):
         pmts(data, Score)

widgets/ic_history.py
@@ -117,23 +117,14 @@ class HistoryWidget(FocusBehavior, Widget):
         return items
 
     def parent_cursor_update(self, data):
-        do_create = data
-
-        if self.data_channel is not None:
-            self.close_channel()
-
-        self.data_channel, do_kickoff = do_create()
-        self.send_to_channel, self.close_channel = self.data_channel.connect(self.receive_from_parent)
-
-        # If we're bound to a different s_cursor in the parent tree, we unconditionally reset our own cursor:
-
-        self.ds = EICHStructure(
+        print("received t-address", data)
+        """
+        self.ds = EHStructure(
             self.ds.score,
-            self._items(self.ds.score),
-            [0],
+            List([n.to_s_expression() for n in self.ds.score.notes()]),
+            [0], # If we're bound to a different s_cursor in the parent tree, we unconditionally reset our own cursor
         )
-
-        do_kickoff()
+        """
 
     def receive_from_parent(self, data):
         pmts(data, Score)

widgets/tree.py
@@ -244,36 +244,7 @@ class TreeWidget(FocusBehavior, Widget):
         self.invalidate()
 
     def broadcast_cursor_update(self, t_address):
-        """
-        The distinction between "somebody moved the cursor" and "the selected _data_ has changed" is relevant for other
-        windows that show something "under the cursor". This is the more so if such windows do not communicate all their
-        changes back to us directly (autosave=False).
-
-        The offered choices / information to the user on what to do with the not yet saved stuff may differ in those 2
-        cases.
-
-        We don't want to expose our address space (t addresses) to our children, but still want to allow for such a
-        distinction. Solution: we broadcast a mechanism `do_create` which may be used to create a data channel.
-        """
-
-        def do_create():
-            channel, send_to_child, close_child = self._child_channel_for_t_address(t_address)
-
-            def do_kickoff():
-                """After the child has connected its listeners to the channel, it wants to know the latest state."""
-                # the s_address is expected to exist (and correct): do_kickoff is assumed to be very quick after the
-                # cursor move... and you cannot move the cursor to a non-existent place.
-                # Regarding using do_kickoff() and do_create() with some time between them: YAGNI.
-                s_address = get_s_address_for_t_address(self.ds.tree, t_address)
-
-                assert s_address is not None
-
-                cursor_node = node_for_s_address(self.ds.tree, s_address)
-                send_to_child(cursor_node.score)
-
-            return channel, do_kickoff
-
-        self.cursor_channel.broadcast(do_create)
+        self.cursor_channel.broadcast(t_address)
 
     def _handle_edit_note(self, edit_note):
         new_s_cursor, not_quite_score, error = edit_note_play(self.ds, edit_note)

editor.py
@@ -86,7 +86,11 @@ class EditorGUI(App):
 
         self.vertical_layout.add_widget(horizontal_layout)
 
+        tree_data_channel, _, _ = tree._child_channel_for_t_address([])
+        tree_data_channel.connect(history_widget.receive_from_parent)
+
         tree.cursor_channel.connect(history_widget.parent_cursor_update)
+
         tree.focus = True
         return tree
 

commit 5b3584a6b7a436fcd08e9b293efe9108139b9808

Fix previous breakage: history-widget follows tree again
Broken in ae8b843b281e; in this fixing commit the histories now
calculate what their "local history" is, that is: the history that's
actually corresponding to the currently selected node on the tree.

Contains ample duplication, this is a simpe consequence of the fact that
I didn't bother to deduplicate communalities between history.py and
ic_history.py yet.

Some additional notes:

* Previous cursor-update was depending on the invalidation in
    update_score, i.e. it only worked by accident; fixed, we always
    reconstruct boxes and invalidate in `parent_cursor_update` now.

* Cursor channel updates always follow score updates. This ensures that
    the cursor can be resolved (if it's pointing to a newly created
    thing).

dsn/history/ic_structure.py
@@ -4,8 +4,9 @@ from utils import pmts
 class EICHStructure(object):
     # Edit In-Context History Structure
 
-    def __init__(self, score, items, s_cursor):
+    def __init__(self, score, items, s_cursor, tree_t_address):
         pmts(items, list)
         self.score = score
         self.items = items
         self.s_cursor = s_cursor
+        self.tree_t_address = tree_t_address

dsn/history/structure.py
@@ -1,6 +1,7 @@
 
 class EHStructure(object):
-    def __init__(self, score, node, s_cursor):
+    def __init__(self, score, node, s_cursor, tree_t_address):
         self.score = score
         self.node = node
         self.s_cursor = s_cursor
+        self.tree_t_address = tree_t_address

widgets/history.py
@@ -11,7 +11,11 @@ from dsn.history.construct import eh_note_play
 from dsn.history.structure import EHStructure
 
 from dsn.s_expr.score import Score
+from dsn.s_expr.construct import play_score
 from dsn.s_expr.structure import Atom, List
+
+from spacetime import get_s_address_for_t_address
+from s_address import node_for_s_address
 
 from dsn.history.clef import (
     EHCursorSet,
@@ -76,7 +80,7 @@ class HistoryWidget(FocusBehavior, Widget):
         # AFAIU:
         # 1. We could basically set any value below.
 
-        self.ds = EHStructure(Score.empty(), List([]), [0])
+        self.ds = EHStructure(Score.empty(), List([]), [0], [])
 
         self.z_pressed = False
         self.viewport_ds = ViewportStructure(
@@ -88,18 +92,37 @@ class HistoryWidget(FocusBehavior, Widget):
         self.bind(size=self.invalidate)
 
     def parent_cursor_update(self, data):
-        print("received t-address", data)
-        """
+        t_address = data
+
+        local_score = self._local_score(self.ds.score, t_address)
         self.ds = EHStructure(
             self.ds.score,
-            List([n.to_s_expression() for n in self.ds.score.notes()]),
-            [0], # If we're bound to a different s_cursor in the parent tree, we unconditionally reset our own cursor
+            List([n.to_s_expression() for n in local_score.notes()]),
+            [0],  # If we're bound to a different s_cursor in the parent tree, we unconditionally reset our own cursor
+            t_address,
         )
-        """
+        self._construct_box_structure()
+        # The desirable behavior is: follow the cursor; Hence: user_moved_cursor=True (even though the history-cursor
+        # moving is only a consequence of cursor move elsewhere)
+        self._update_viewport_for_change(user_moved_cursor=True)
+        self.invalidate()
 
     def receive_from_parent(self, data):
         pmts(data, Score)
         self.update_score(data)
+
+    def _local_score(self, score, tree_t_address):
+        tree = play_score(self.m, score)
+        s_address = get_s_address_for_t_address(tree, tree_t_address)
+
+        if s_address is None:
+            # because changes to the Score and the Cursor are not communicated to us in a transactional way, deletions
+            # in the tree will lead to a (temporarily) invalid cursor. We set our own score to the empty one in that
+            # case; the cursor_update that follows right after will restore the situation
+            return Score.empty()
+
+        cursor_node = node_for_s_address(tree, s_address)
+        return cursor_node.score
 
     def update_score(self, score):
         # For each "tree cursor" change, we reset our own cursor to the end (most recent item)
@@ -107,19 +130,23 @@ class HistoryWidget(FocusBehavior, Widget):
         # lowest possible position; this is something to be aware of because it might cause some confusion). At some
         # point this may be automatically solved, e.g. if we change the behavior of cursor-following to be "follow the
         # cursor, making sure it is in-view including the recursive subparts).
-        s_cursor = [len(score) - 1]
+        local_score = self._local_score(score, self.ds.tree_t_address)
+        s_cursor = [len(local_score) - 1]
 
         self.ds = EHStructure(
             score,
-            List([n.to_s_expression() for n in score.notes()]),
+            List([n.to_s_expression() for n in local_score.notes()]),
             s_cursor,
+            self.ds.tree_t_address,
         )
 
         self._construct_box_structure()
 
-        # If nout_hash update results in a cursor-reset, the desirable behavior is: follow the cursor; if the cursor
-        # remains the same, the value of user_moved_cursor doesn't matter. Hence: user_moved_cursor=True
-        self._update_viewport_for_change(user_moved_cursor=True)
+        if len(local_score) > 0:  # guard against "no reasonable cursor, hence no reasonable viewport change"
+            # If nout_hash update results in a cursor-reset, the desirable behavior is: follow the cursor; if the cursor
+            # remains the same, the value of user_moved_cursor doesn't matter. Hence: user_moved_cursor=True
+            self._update_viewport_for_change(user_moved_cursor=True)
+
         self.invalidate()
 
     def _handle_eh_note(self, eh_note):

widgets/ic_history.py
@@ -12,6 +12,9 @@ from dsn.history.ic_structure import EICHStructure
 
 from dsn.s_expr.score import Score
 
+from spacetime import get_s_address_for_t_address
+from s_address import node_for_s_address
+
 from dsn.history.clef import (
     EHCursorSet,
     #     EHCursorChild,
@@ -85,7 +88,7 @@ class HistoryWidget(FocusBehavior, Widget):
         # AFAIU:
         # 1. We could basically set any value below.
 
-        self.ds = EICHStructure(Score.empty(), [], [0])
+        self.ds = EICHStructure(Score.empty(), [], [0], [])
 
         self.z_pressed = False
         self.viewport_ds = ViewportStructure(
@@ -117,18 +120,36 @@ class HistoryWidget(FocusBehavior, Widget):
         return items
 
     def parent_cursor_update(self, data):
-        print("received t-address", data)
-        """
-        self.ds = EHStructure(
+        t_address = data
+        self.ds = EICHStructure(
             self.ds.score,
-            List([n.to_s_expression() for n in self.ds.score.notes()]),
-            [0], # If we're bound to a different s_cursor in the parent tree, we unconditionally reset our own cursor
+            self._items(self._local_score(self.ds.score, t_address)),
+            [0],  # If we're bound to a different s_cursor in the parent tree, we unconditionally reset our own cursor
+            t_address,
         )
-        """
+
+        self._construct_box_structure()
+        # The desirable behavior is: follow the cursor; Hence: user_moved_cursor=True (even though the history-cursor
+        # moving is only a consequence of cursor move elsewhere)
+        self._update_viewport_for_change(user_moved_cursor=True)
+        self.invalidate()
 
     def receive_from_parent(self, data):
         pmts(data, Score)
         self.update_score(data)
+
+    def _local_score(self, score, tree_t_address):
+        tree = play_score_regularly(self.m, score)
+        s_address = get_s_address_for_t_address(tree, tree_t_address)
+
+        if s_address is None:
+            # because changes to the Score and the Cursor are not communicated to us in a transactional way, deletions
+            # in the tree will lead to a (temporarily) invalid cursor. We set our own score to the empty one in that
+            # case; the cursor_update that follows right after will restore the situation
+            return Score.empty()
+
+        cursor_node = node_for_s_address(tree, s_address)
+        return cursor_node.score
 
     def update_score(self, score):
         # For each "tree cursor" change, we reset our own cursor to the end (most recent item)
@@ -136,19 +157,24 @@ class HistoryWidget(FocusBehavior, Widget):
         # lowest possible position; this is something to be aware of because it might cause some confusion). At some
         # point this may be automatically solved, e.g. if we change the behavior of cursor-following to be "follow the
         # cursor, making sure it is in-view including the recursive subparts).
-        s_cursor = [len(score) - 1]
+
+        local_score = self._local_score(score, self.ds.tree_t_address)
+        s_cursor = [len(local_score) - 1]
 
         self.ds = EICHStructure(
             score,
-            self._items(score),
+            self._items(local_score),
             s_cursor,
+            self.ds.tree_t_address,
         )
 
         self._construct_box_structure()
 
-        # If nout_hash update results in a cursor-reset, the desirable behavior is: follow the cursor; if the cursor
-        # remains the same, the value of user_moved_cursor doesn't matter. Hence: user_moved_cursor=True
-        self._update_viewport_for_change(user_moved_cursor=True)
+        if len(local_score) > 0:  # guard against "no reasonable cursor, hence no reasonable viewport change"
+            # If nout_hash update results in a cursor-reset, the desirable behavior is: follow the cursor; if the cursor
+            # remains the same, the value of user_moved_cursor doesn't matter. Hence: user_moved_cursor=True
+            self._update_viewport_for_change(user_moved_cursor=True)
+
         self.invalidate()
 
     def _handle_eh_note(self, eh_note):

widgets/tree.py
@@ -229,10 +229,6 @@ class TreeWidget(FocusBehavior, Widget):
             construct_pp_tree(tree, self.ds.pp_annotations)
         )
 
-        # TODO we only really need to broadcast the new t_cursor if it has changed (e.g. because the previously
-        # selected t_cursor is no longer valid)
-        self.broadcast_cursor_update(t_address_for_s_address(self.ds.tree, self.ds.s_cursor))
-
         self._update_selection_ds_for_main_ds()
         self._construct_box_structure()
         self._update_viewport_for_change(user_moved_cursor=False)
@@ -240,6 +236,10 @@ class TreeWidget(FocusBehavior, Widget):
 
         for notify_child in self.notify_children.values():
             notify_child()  # (data)
+
+        # TODO we only really need to broadcast the new t_cursor if it has changed (e.g. because the previously
+        # selected t_cursor is no longer valid)
+        self.broadcast_cursor_update(t_address_for_s_address(self.ds.tree, self.ds.s_cursor))
 
     def channel_closed(self):
         self.closed = True
@@ -312,9 +312,6 @@ class TreeWidget(FocusBehavior, Widget):
         )
         self._update_selection_ds_for_main_ds()
 
-        # TODO we only really need to broadcast the new t_cursor if it has changed.
-        self.broadcast_cursor_update(t_address_for_s_address(self.ds.tree, self.ds.s_cursor))
-
         self._update_selection_ds_for_main_ds()
         self._construct_box_structure()
         self._update_viewport_for_change(user_moved_cursor=user_moved_cursor)
@@ -322,6 +319,9 @@ class TreeWidget(FocusBehavior, Widget):
 
         for notify_child in self.notify_children.values():
             notify_child()  # (score)
+
+        # TODO we only really need to broadcast the new t_cursor if it has changed.
+        self.broadcast_cursor_update(t_address_for_s_address(self.ds.tree, self.ds.s_cursor))
 
     def keyboard_on_textinput(self, window, text):
         FocusBehavior.keyboard_on_textinput(self, window, text)